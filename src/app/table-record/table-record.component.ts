9b61a9e8685921228c17976db881e37986614753
EfratIfergan <efrat3888@gmail.com>
2023-03-15 15:24:40 +0200

    Upload all the files.

---
+++ src/app/table-record/table-record.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-record',
+  templateUrl: './table-record.component.html',
+  styleUrls: ['./table-record.component.scss']
+})
+export class TableRecordComponent {
+
+}
